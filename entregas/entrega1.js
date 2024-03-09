27ceb6090efc760046411f81d48839f33e93e8f8
Ana Valentin Rodriguez <anvarod1@upv.edu.es>
2024-03-09 20:22:15 +0100

    juego completo

--- entregas/entrega1.js
+++ entregas/entrega1.js
@@ -10,15 +10,16 @@
 import * as THREE from "../lib/three.module.js";
 import { OrbitControls } from "../lib/OrbitControls.module.js";
 import { TWEEN } from "../lib/tween.module.min.js";
+import { GUI } from "../lib/lil-gui.module.min.js";
 
 // Variables de consenso
-let renderer, scene, camera;
+let renderer, scene, camera, focal;
 
 // Otras globales
 /*******************
  * TO DO: Variables globales de la aplicacion
  *******************/
-let cameraControls, effectController;
+let cameraControls, effectController, estadoCamara;
 let whiteInitialNames = ['w_peon1', 'w_peon2', 'w_peon3', 'w_peon4', 'w_peon5', 'w_peon6', 'w_peon7', 'w_peon8', 'w_alfil1', 'w_alfil2', 'w_tower1', 'w_tower2', 'w_caballo1', 'w_caballo2', 'w_reina', 'w_rey'];
 let blackInitialNames = ['b_peon1', 'b_peon2', 'b_peon3', 'b_peon4', 'b_peon5', 'b_peon6', 'b_peon7', 'b_peon8', 'b_alfil1', 'b_alfil2', 'b_tower1', 'b_tower2', 'b_caballo1', 'b_caballo2', 'b_reina', 'b_rey'];
 
@@ -37,9 +38,13 @@ var sueloMaterial = new THREE.MeshBasicMaterial({ color: 0x2a382a });
 
 const ultimaCasilla = -2.975;
 const primeraCasilla = -0;
+
+const brilloFichas = 30;
+const brilloCasillas = 15;
 // Acciones
 init();
 loadScene();
+loadGUI();
 render();
 
 function init() {
@@ -59,19 +64,75 @@ function init() {
 
     // Camara
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(4, 3, 6);
-    /*******************
-    * TO DO: Añadir manejador de camara (OrbitControls)
-    *******************/
+    camera.position.set(3, 8, 0);
     cameraControls = new OrbitControls(camera, renderer.domElement);
     cameraControls.target.set(0, 1, 0);
     camera.lookAt(new THREE.Vector3(0, 1, 0));
 
+    renderer.domElement.addEventListener('dblclick', animate);
+    renderer.domElement.addEventListener('click', selectPosition);
+}
+
+function mirarBlancas() {
+    new TWEEN.Tween(estadoCamara)
+        .to({ x: 3, y: 8, z: 0 }, 2000)
+        .easing(TWEEN.Easing.Quadratic.InOut) // Utiliza un easing para la transición suave
+        .onUpdate(function () {
+            // Actualiza la posición de la cámara en cada fotograma de la animación
+            camera.position.set(estadoCamara.x, estadoCamara.y, estadoCamara.z);
+
+            camera.lookAt(new THREE.Vector3(0, 1, -0.01));
+        })
+        .start();
+
+    new TWEEN.Tween(cameraControls.target)
+        .to({ x: 0, y: 1, z: 0 }, 2000)
+        .easing(TWEEN.Easing.Quadratic.InOut) // Utiliza un easing para la transición suave
+        .onUpdate(function () {
+            // Actualiza la posición de la cámara en cada fotograma de la animación
+            camera.position.set(estadoCamara.x, estadoCamara.y, estadoCamara.z);
+        })
+        .start();
+
+}
+
+function mirarNegras() {
+    new TWEEN.Tween(estadoCamara)
+        .to({ x: -3, y: 8, z: 0 }, 3000)
+        .easing(TWEEN.Easing.Quadratic.InOut) // Utiliza un easing para la transición suave
+        .onUpdate(function () {
+            // Actualiza la posición de la cámara en cada fotograma de la animación
+            camera.position.set(estadoCamara.x, estadoCamara.y, estadoCamara.z);
+
+            camera.lookAt(new THREE.Vector3(0, 1, 0.01));
+        })
+        .start();
+
+    new TWEEN.Tween(cameraControls.target)
+        .to({ x: 0, y: 1, z: 0 }, 3000)
+        .easing(TWEEN.Easing.Quadratic.InOut) // Utiliza un easing para la transición suave
+        .onUpdate(function () {
+            // Actualiza la posición de la cámara en cada fotograma de la animación
+            camera.position.set(estadoCamara.x, estadoCamara.y, estadoCamara.z);
+        })
+        .start();
+}
+
+function loadScene() {
+    turno = 0;
+    scene.children = [];
+    whiteNames = whiteInitialNames;
+    blackNames = blackInitialNames;
+    winner = '';
+    casillas = [];
+    fichaSeleccionada = { name: '' };
+    casillasPosibles = [];
+    casillasPosiblesKill = [];
     // Crear un material para el plano
     var geometry = new THREE.PlaneGeometry(10000, 10000);
     suelo = new THREE.Mesh(geometry, sueloMaterial);
     suelo.rotation.x = -Math.PI / 2;
-    suelo.position.y = -1.02;
+    suelo.position.y = -1.05;
     scene.add(suelo);
 
     // Luces
@@ -82,66 +143,46 @@ function init() {
     direccional.castShadow = true;
     scene.add(direccional);
     const puntual = new THREE.PointLight(0xFFFFFF, 0.5);
-    puntual.position.set(2, 7, -4);
+    puntual.position.set(4, 10, 4);
     scene.add(puntual);
-    const focal = new THREE.SpotLight(0xFFFFFF, 0.3);
-    focal.position.set(-2, 7, 4);
+    focal = new THREE.SpotLight(0xFFFFFF, 0.3);
+    focal.position.set(-6, 7, 2);
     focal.target.position.set(0, 0, 0);
     focal.angle = Math.PI / 7;
     focal.penumbra = 0.3;
     focal.castShadow = true;
     focal.shadow.camera.far = 20;
     focal.shadow.camera.fov = 80;
+
     scene.add(focal);
+
+    // scene.add(new THREE.CameraHelper(focal.shadow.camera));
 
     // Cargar la textura del gradiente
     const textureLoader = new THREE.TextureLoader();
     const gradientTexture = textureLoader.load('entregas/scene_e1/floor.jpg');
-
-    // Crear un material utilizando la textura del gradiente
     const gradientMaterial = new THREE.MeshBasicMaterial({ map: gradientTexture });
-
-    // Crear un plano grande para el fondo
-    const gradientGeometry = new THREE.PlaneGeometry(10, 10); // Ajusta el tamaño según tu escena
-
-    // Crear la malla del plano con la textura del gradiente
+    const gradientGeometry = new THREE.PlaneGeometry(10, 10);
     const gradientMesh = new THREE.Mesh(gradientGeometry, gradientMaterial);
-
-    // Rotar el plano para que sea el fondo
-    gradientMesh.rotation.x = Math.PI * -0.5; // Rota 90 grados en sentido antihorario
-
-    // Ajusta la posición del plano para que esté detrás de otros objetos
-    gradientMesh.position.y = -1; // Ajusta la altura según tu escena
-
-    // Añadir el plano con la textura del gradiente a la escena
+    gradientMesh.rotation.x = Math.PI * -0.5;
+    gradientMesh.position.y = -1;
     scene.add(gradientMesh);
-    renderer.domElement.addEventListener('dblclick', animate);
 
-    renderer.domElement.addEventListener('click', selectPosition);
-}
-
-function loadScene() {
-    /*******************
-    * TO DO: Misma escena que en la practica anterior
-    *******************/
-    const textureLoader = new THREE.TextureLoader();
-    const gradientTexture = textureLoader.load('entregas/scene_e1/concrete.jpg');
+    const textureLoader2 = new THREE.TextureLoader();
+    const gradientTexture2 = textureLoader2.load('entregas/scene_e1/concrete.jpg');
     let material = new THREE.MeshStandardMaterial({
-        map: gradientTexture, // Tu textura
+        map: gradientTexture2, // Tu textura
         roughness: 0.7, // Ajusta la rugosidad según tus necesidades
         metalness: 0 // Ajusta la metalicidad según tus necesidades
     });
-    let materialWhite = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: 20 });
-    let materialBlack = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: 20 });
-
-
+    let materialWhite = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: brilloCasillas });
+    let materialBlack = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: brilloCasillas });
 
     const loader = new THREE.CubeTextureLoader();
     const cubemap = loader.load(["entregas/scene_e1/posx.jpg", "entregas/scene_e1/negx.jpg",
         "entregas/scene_e1/posy.jpg", "entregas/scene_e1/negy.jpg",
         "entregas/scene_e1/posz.jpg", "entregas/scene_e1/negz.jpg"]);
     scene.background = cubemap;
-
 
     const mesa = loadMesa(material);
     const tablero = loadTablero(material, materialWhite, materialBlack);
@@ -553,7 +594,7 @@ function loadCaballo(material, posX, posY, north) {
 }
 
 function loadWhite() {
-    let white = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: 30 });
+    let white = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: brilloFichas });
 
     let list = [];
     for (let i = 0; i < 8; i++) {
@@ -595,7 +636,7 @@ function loadWhite() {
 }
 
 function loadBlack() {
-    let black = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: 30 });
+    let black = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: brilloFichas });
 
     let list = [];
     for (let i = 0; i < 8; i++) {
@@ -791,13 +832,13 @@ function selectFicha(ficha) {
             let f = scene.getObjectByName(e);
             if (f) {
                 f.children.forEach((e) => {
-                    e.material = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: 30 });
+                    e.material = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: brilloFichas });
                 })
             }
         } else {
             fichaSeleccionada = ficha;
             ficha.children.forEach((e) => {
-                e.material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: 30 });
+                e.material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: brilloFichas });
             })
         }
     });
@@ -808,13 +849,13 @@ function selectFicha(ficha) {
             let f = scene.getObjectByName(e);
             if (f) {
                 f.children.forEach((e) => {
-                    e.material = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: 30 });
+                    e.material = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: brilloFichas });
                 })
             }
         } else {
             fichaSeleccionada = ficha;
             ficha.children.forEach((e) => {
-                e.material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: 30 });
+                e.material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: brilloFichas });
             })
         }
     });
@@ -823,8 +864,8 @@ function selectFicha(ficha) {
 }
 
 function resetTablero() {
-    let white = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: 30 });
-    let black = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: 30 });
+    let white = new THREE.MeshPhongMaterial({ color: 'white', specular: '#f0f0f0', shininess: brilloCasillas });
+    let black = new THREE.MeshPhongMaterial({ color: 'black', specular: '#f0f0f0', shininess: brilloCasillas });
     let colors = [white, black];
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
@@ -838,54 +879,64 @@ function resetTablero() {
 
 function getPeonPositions(ficha) {
     let positions = [];
+    let killPositions = [];
+    let fichaEncontrada = false;
+    let initialPos;
+    let i = 1;
     if (ficha.name.includes('w_')) {
-        positions.push({ x: ficha.position.x - 0.425, z: ficha.position.z });
-        //cuando está al inicio y puede avanzar dos casillas a la vez
-        if (ficha.position.x == -0.425) {
-            positions.push({ x: ficha.position.x - (0.425 * 2), z: ficha.position.z });
+        initialPos = { x: ficha.position.x - 0.425, z: ficha.position.z }
+        let existe = existeFichaEnPosicion(initialPos.x, initialPos.z);
+        if (existe) {
+            fichaEncontrada = true;
         }
-    } else {
-        positions.push({ x: ficha.position.x + 0.425, z: ficha.position.z });
-        //cuando está al inicio y puede avanzar dos casillas a la vez
-        if (ficha.position.x == -2.55) {
-            positions.push({ x: ficha.position.x + (0.425 * 2), z: ficha.position.z });
-        }
-    }
-
-    let casillasSeleccionadas = [];
-    //quitar todas las posiciones que, una vez encontrada una ficha en cualquier dirección, no pueda seguir avanzando
-    for (let i = 0; i < casillas.length; i++) {
-        let casilla = scene.getObjectByName('tablero').getObjectByName(casillas[i]).parent;
-        for (let p = 0; p < positions.length; p++) {
-            if (positions[p].x.toFixed(1) == casilla.position.x.toFixed(1) && positions[p].z.toFixed(1) == casilla.position.z.toFixed(1)) {
-                let existeFicha = false;
-                for (let j = 0; j < whiteNames.length; j++) {
-                    let f = scene.getObjectByName(whiteNames[j]);
-                    if (f.position.x == casilla.position.x && f.position.z == casilla.position.z) {
-                        positions.splice(p, positions.length - p);
-                        existeFicha = true;
-                    }
-                }
-                for (let j = 0; j < blackNames.length; j++) {
-                    let f = scene.getObjectByName(blackNames[j]);
-                    if (f.position.x == casilla.position.x && f.position.z == casilla.position.z) {
-                        positions.splice(p, positions.length - p);
-                        existeFicha = true;
-                    }
-                }
-                if (!existeFicha) {
-                    casillasSeleccionadas.push(casilla)
+        if (!fichaEncontrada) {
+            positions.push(initialPos);
+            if (ficha.position.x == primeraCasilla - 0.425) {
+                initialPos = { x: ficha.position.x - (0.425 * 2), z: ficha.position.z }
+                existe = existeFichaEnPosicion(initialPos.x, initialPos.z);
+                if (!existe) {
+                    positions.push(initialPos);
                 }
             }
         }
-    }
-    resetTablero();
-    casillasSeleccionadas.forEach((e) => {
-        e.children[0].material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: 30 });
-    })
-    casillasPosibles = casillasSeleccionadas;
-    return positions;
 
+        let killIzq = { x: ficha.position.x - 0.425, z: ficha.position.z + 0.425 }
+        let killDer = { x: ficha.position.x - 0.425, z: ficha.position.z - 0.425 }
+        if (existeFichaEnPosicion(killIzq.x, killIzq.z).kill) {
+            killPositions.push(killIzq);
+        }
+        if (existeFichaEnPosicion(killDer.x, killDer.z).kill) {
+            killPositions.push(killDer);
+        }
+    } else {
+        initialPos = { x: ficha.position.x + 0.425, z: ficha.position.z }
+        let existe = existeFichaEnPosicion(initialPos.x, initialPos.z);
+        if (existe) {
+            fichaEncontrada = true;
+        }
+        if (!fichaEncontrada) {
+            positions.push(initialPos);
+            if (ficha.position.x.toFixed(2) == (ultimaCasilla + 0.425).toFixed(2)) {
+                initialPos = { x: ficha.position.x + (0.425 * 2), z: ficha.position.z }
+                existe = existeFichaEnPosicion(initialPos.x, initialPos.z);
+                if (!existe) {
+                    positions.push(initialPos);
+                }
+            }
+        }
+
+        let killIzq = { x: ficha.position.x + 0.425, z: ficha.position.z + 0.425 }
+        let killDer = { x: ficha.position.x + 0.425, z: ficha.position.z - 0.425 }
+        if (existeFichaEnPosicion(killIzq.x, killIzq.z).kill) {
+            killPositions.push(killIzq);
+        }
+        if (existeFichaEnPosicion(killDer.x, killDer.z).kill) {
+            killPositions.push(killDer);
+        }
+    }
+
+    seleccionaCasillas(positions, killPositions);
+    return positions;
 }
 
 function getTowerPositions(ficha) {
@@ -1199,10 +1250,10 @@ function seleccionaCasillas(positions, killPositions = []) {
     }
     resetTablero();
     casillasSeleccionadas.forEach((e) => {
-        e.children[0].material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: 30 });
+        e.children[0].material = new THREE.MeshPhongMaterial({ color: 'red', specular: '#f0f0f0', shininess: brilloCasillas });
     })
     casillasKill.forEach((e) => {
-        e.children[0].material = new THREE.MeshPhongMaterial({ color: 'purple', specular: '#f0f0f0', shininess: 30 });
+        e.children[0].material = new THREE.MeshPhongMaterial({ color: 'purple', specular: '#f0f0f0', shininess: brilloCasillas });
     })
     casillasPosibles = casillasSeleccionadas;
     casillasPosiblesKill = casillasKill;
@@ -1230,26 +1281,52 @@ function updateTurno() {
         console.log('turno', turno);
         turno++;
         scene.getObjectByName('bordes').children.forEach((e) => {
-            e.material = new THREE.MeshPhongMaterial({ color: turno%2? 'black' : 'white',  specular: '#f0f0f0', shininess: 30 });
+            e.material = new THREE.MeshPhongMaterial({ color: turno % 2 ? 'black' : 'white', specular: '#f0f0f0', shininess: brilloFichas });
         }
         );
     }
 }
 
-function updateWinner(){
-    if(!whiteNames.find((name)=>name.includes('rey'))){
+function updateWinner() {
+    if (!whiteNames.find((name) => name.includes('rey'))) {
         winner = 'b';
-        suelo.material = new THREE.MeshStandardMaterial({ color:'black', roughness:0.7 });
-    }else if(!blackNames.find((name)=>name.includes('rey'))){
+        suelo.material = new THREE.MeshStandardMaterial({ color: 'black', roughness: 0.7 });
+    } else if (!blackNames.find((name) => name.includes('rey'))) {
         winner = 'w';
-        suelo.material = new THREE.MeshStandardMaterial({ color:'white', roughness:0.7 });
+        suelo.material = new THREE.MeshStandardMaterial({ color: 'white', roughness: 0.7 });
     }
 }
+
+function loadGUI() {
+    effectController = {
+        vistaBlancas: () => {
+            mirarBlancas();
+        },
+        vistaNegras: () => {
+            mirarNegras();
+        },
+        restart: () => {
+            loadScene();
+        }
+    };
+
+    // Creacion interfaz
+    const gui = new GUI();
+
+    // Construccion del menu
+    const h = gui.addFolder("Control vistas");
+    h.add(effectController, "vistaBlancas").name("Vista blancas");
+    h.add(effectController, "vistaNegras").name("Vsita negras");
+    h.add(effectController, "restart").name("Volver a empezar");
+
+}
+
 function update(delta) {
     /*******************
     * TO DO: Actualizar tween
     *******************/
     TWEEN.update();
+    estadoCamara = camera.position;
 }
 
 function render(delta) {
